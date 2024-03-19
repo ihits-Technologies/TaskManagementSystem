37068f80c9ee02b71b3db301828483a2bc9caba6
riyasfath <riyasfath@gmail.com>
2024-03-19 14:10:32 +0530

    some files missed

--- lib/services/firebase/firebase_db.dart
+++ lib/services/firebase/firebase_db.dart
@@ -1,33 +1,33 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-final FirebaseFirestore firestore = FirebaseFirestore.instance;
-Future<DocumentReference> creatChat(
-    String chatId, Map<String, bool> participants) async {
-  final chatRef = await firestore.collection('chats').add({
-    'participants': participants,
-  });
-  return chatRef;
-}
-
-Future<void> addMessage(
-    String chatId, String messageId, String senderId, String text) async {
-  final messageRef = await firestore
-      .collection('chats')
-      .doc(chatId)
-      .collection('messages')
-      .doc(messageId)
-      .set({
-    'senderId': senderId,
-    'text': text,
-    "timestamp": FieldValue.serverTimestamp(),
-  });
-}
-Future<void> main() async{
-  /////create new chat////
-  final chatRef = await creatChat('chatId', {'user1': true, 'user2': true} );
-
-  /// add message to the chat//
-  await addMessage(chatRef.id, 'messageId1', 'user1', 'Hi there');
-  await addMessage(chatRef.id, 'messageId2', 'user2', 'Hi hw arrrr yuoo');
-
-}
+// import 'package:cloud_firestore/cloud_firestore.dart';
+//
+// final FirebaseFirestore firestore = FirebaseFirestore.instance;
+// Future<DocumentReference> creatChat(
+//     String chatId, Map<String, bool> participants) async {
+//   final chatRef = await firestore.collection('chats').add({
+//     'participants': participants,
+//   });
+//   return chatRef;
+// }
+//
+// Future<void> addMessage(
+//     String chatId, String messageId, String senderId, String text) async {
+//   final messageRef = await firestore
+//       .collection('chats')
+//       .doc(chatId)
+//       .collection('messages')
+//       .doc(messageId)
+//       .set({
+//     'senderId': senderId,
+//     'text': text,
+//     "timestamp": FieldValue.serverTimestamp(),
+//   });
+// }
+// Future<void> main() async{
+//   /////create new chat////
+//   final chatRef = await creatChat('chatId', {'user1': true, 'user2': true} );
+//
+//   /// add message to the chat//
+//   await addMessage(chatRef.id, 'messageId1', 'user1', 'Hi there');
+//   await addMessage(chatRef.id, 'messageId2', 'user2', 'Hi hw arrrr yuoo');
+//
+// }
